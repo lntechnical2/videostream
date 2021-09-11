cd8ff92646ca6f139e50fd045832df8801375491
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-09-11 11:25:38 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,1 +1,17 @@
+from pyrogram import Client, idle
+import os
+from Bot.video_stream import app
+API_ID = os.environ.get("API_ID",12345)
+API_HASH = os.environ.get("API_HASH","")
+TOKEN = os.environ.get("TOKEN","")
 
+bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=TOKEN,
+    plugins=dict(root="Bot"),
+)
+bot.start()
+app.start()
+idle()

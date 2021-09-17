502dc1b32118d5699f0f55df03a2563edd460918
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-09-17 18:17:45 +0530

    Create group_strem.py

---
+++ Bot/group_strem.py
@@ -0,0 +1,66 @@
+import re 
+import os
+from pytgcalls import GroupCallFactory
+from pyrogram import Client, filters
+from pyrogram.types import Message
+# from py_youtube import ytdl
+from py_youtube import ytdl ,Data
+
+from video_stream import app
+
+group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM)
+VIDEO_CALL = {}
+
+
+
+@Client.on_message(filters.group & filters.command(["play"]))
+async def play(client, m: Message):
+	if (m.reply_to_message):
+			get =await client.get_chat_member(message.chat.id,message.from_user.id)
+			status = get. status
+			cmd_user = ["administrator","creator"]
+			if status in cmd_user:
+				     link = m.reply_to_message.text
+				     youtube_regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+"
+				     youtube_regex_match = re.match(youtube_regex, link)
+				     if youtube_regex_match:
+				     	try:
+				     	       data = Data(link).data()
+				     	       video_url = ytdl(link).besturl()
+				     	except Exception as e:
+				     	       await m.reply(f"**Error** -- `{e}`")
+				     	       return
+				     	try:
+				     		group_call = group_call_factory.get_group_call()
+				     		await group_call.join(message.chat.id)
+				     		await group_call.start_video(video_url,enable_experimental_lip_sync=True)
+				     		VIDEO_CALL[message.chat.id] = group_call
+				     		await client.send_photo(m.chat.id,photo=data["thumbnails"],caption =f"**Title :{data['title']}**\n**Views :{data['views']}**\n**Likes : {data['likes']}**",reply_to_message_id=m.message_id)
+				     	
+				     	except Exception as e:
+				         	await m.reply(f"**Error** -- `{e}`")
+				     else:
+			         	try:
+			         		group_call = group_call_factory.get_group_call()
+			         		await group_call.join(CHAT)
+			         		await group_call.start_video(link,enable_experimental_lip_sync=True)
+			         		VIDEO_CALL[CHAT] = group_call
+			         		await m.reply("** Started Streaming!**")
+			         	except Exception as e:
+			         	    	await m.reply(f"**Error** -- `{e}`")
+				             	
+					
+			
+				             	
+
+@Client.on_message(filters.group & filters.command(["stop"]))
+async def stop (client, m: Message):
+	get =await client.get_chat_member(message.chat.id,message.from_user.id)
+	status = get. status
+	cmd_user = ["administrator","creator"]
+	if status in cmd_user:
+	       try:
+	       	await VIDEO_CALL[message.chat.id].stop()
+	       	await m.reply("** Stopped Streaming!**")
+	       except Exception as e:
+	       	await m.reply(f"**Error** - `{e}`")
